d4522f3d45217ff2c0826aa69c9faf22a4b2460c
spencer <Soclemens@gmail.com>
2021-06-13 15:04:50 -0600

    updated the main.py file to reflect the project design a little closer and add a bunch of todos for us to work on.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,26 @@
-from src.env import *
-from src.agent import *
+from env import *
+from agent import *
 
 def initialize():
     # set up
     gameLoop()
 
 def gameLoop():
+    theMarket = StockMarket()
+    # TODO: create a list of actors
     while True:
-        # get perset for the day, give it to agents
-        # get actions from agents, pass those actions to evn
-        # return prices from evn to agents
-        # fluctuate stocks for the day
+        # TODO: Give each actor a chance to act based on that days perceptions. The list of actions ->
+        #           Check my holdings and determine if I want to sell
+        #               if yes update stock sell count and log this action under the actors log
+        #           Check each stock and determine whether I want to buy any *can only own one of each **maybe
+        #               if yes update stock buys count and log this action under the actors log
+        #           Update my current returns and standings in the leader board
+        # TODO: Give the market a chance to update itself for the next round
         break
+
+    # TODO: Report final standings
 
 
 
 if __name__ == 'main':
     initialize()
-    agent = RiskNeutral()
-    agent.act()
-    evn = StockMarket()
