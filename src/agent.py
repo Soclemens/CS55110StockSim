a5a1a7c1234645fecc7968dc77ea4c5308410272
Colton Hill <colton.hill@aggiemail.usu.edu>
2021-06-15 01:59:40 -0600

    Stripped down the mass ordering for simple purchasing (much crying)

--- src/agent.py
+++ src/agent.py
@@ -2,63 +2,122 @@
 class Trader:
     """Abstract class to allow for interfacing with env
        Contains act() and a default constructor"""
-    def __init__(self, stockNumber, money=10_000, stopLost=0.25) -> None:
-        self.__stockLimit
+
+    #NOTE: Not used. Only 20 stocks are permitted, 1 of each stock
+    #NOTE: Currently, there is not debt limit
+    def __init__(self, alltheStocks, money=10_000, stopLost=0.2, name="ABSTRACT") -> None:
+        self.name = name  # Dump little ID for pretty printing the type of agent for the report
         self.__money = money
-        self.__deptLimit = money  # yoy can only go -10,000 in dept
+        # self.__deptLimit = money  # yoy can only go -10,000 in dept
         # list of stokes, amount, & their purchase price
-        self.__holdings = { i:{'stock':None, 'amount':0, 'purchasePrice':0} for i in range(stockNumber)}  
+        self.__holdings = { stock:{'amount':0,'purchasePrice':0} for stock in alltheStocks}  
         self.__log = []  # logs of actions taken
-        self.__stopLoss = stopLost  # persentage return. TODO, figure out of this should be flat, or percentile
+        self.__stopLoss = stopLost  # persentage of return I want
+        # self.__stockLimit = 20
 
-    def act(self, listings:list) -> list:
+    def netWorth(self):
+        return self.__money + sum([stock.goingPrice() * self.__holdings[stock]['amount'] for stock in self.__holdings.keys()])
+        
+    def print(self, showPortfolio = False) -> str:
         """
-        Returns an "action" or a list of stock/number pairs corresponding to the by/sell amount
+        for printing the agent with its Type, Networth, then its portfolio of stocks with each price
         """
-        stockOrder = {}
-        for listing in listings:
-            stockOrder[listing.key] = self.analyzeStock(listing)
+        msg = f"Agent:{self.name}\t"+ "NetWorth:" + format(self.netWorth(),".2f") +"\n\t"
+        if showPortfolio:
+            msg += "\n\t".join([
+                "stock:"+stock.name+", "+format(stock.goingPrice(),".2f") 
+                for stock in self.__holdings.keys() if self.__holdings[stock]['amount']>0
+            ])
+        print(msg)
 
-        # TODO: Do I mamage the amount of stacks
-        # How do I Balance the amount of
-        # WHen do I remove stocks
-        # when is it okay to remove.
+    def act(self, stock):
+        """
+        Currently, just and interface to log the agents action based on it's stock analysis
+        """
+        result = self.__analyzeStock(stock)
+        self.__log.append(result[1])
+        return result
 
-        return [("NAN", 0)]
-
-    def analyzeStock(self, stock):  # no history used... yet
-        ratio = stock.__goingPrice()/self.__holdings.get(stock.key,default={})
-
-    def adjustBalance(self, money):
-        self.__money += money
+    # NOTE: This automatically assumes it's buying/selling its stock, make sure the market reflects that
+    # NOTE: I have no idea what needs to be returned, the string or bool, or both.
+    def __analyzeStock(self, stock) -> tuple:
+        """
+        This is the "act" function, returns true if they want to buy/keep a stock,
+            or false if they want to sell/pass on that same stock
+        """
+        if self.__holdings[stock]['amount']>0:# Check if I already own this stock
+            # check if I'm withing the stock tolerance
+            if abs(1-stock.goingPrice()/self.__holdings[stock]['purchasePrice'])>self.__stopLoss:
+                # Sell
+                stock.stockSold()
+                self.__holdings[stock]['amount'] -= 1  # remove from my own ledger
+                # print(self.__holdings[stock]['amount'])  # add to my own ledger
+                self.__money += stock.goingPrice()  # pocket the windfall
+                self.__holdings[stock]['purchasePrice'] = 0
+                return False, "Sell"
+            else:
+                return True, "Keep"
+        else:  # nope, that's new
+            if stock.volatility()<=self.__stopLoss: # check volatility
+                # Buy
+                stock.stockBought()
+                self.__holdings[stock]['amount'] += 1  # add to my own ledger
+                # print(self.__holdings[stock]['amount'])  # add to my own ledger
+                self.__money -= stock.goingPrice()  # deduct the price
+                self.__holdings[stock]['purchasePrice'] = stock.goingPrice()
+                return True, "Buy"
+            else:
+                return False, "Pass" 
 
     
+    # NOTE: currently deprecate, might bring back if we revert to mass ordering
+    # def act(self, listings:list) -> list:
+    #     """
+    #     Returns an "action" or a list of stock/number pairs corresponding to the by/sell amount
+    #     """
+    #     stockOrder = {}
+    #     for listing in listings:
+    #         stockOrder[listing.key] = self.analyzeStock(listing)
+
+    #     # Do I mamage the amount of stacks
+    #     # How do I Balance the amount of
+    #     # WHen do I remove stocks
+    #     # when is it okay to remove.
+    
+    #    return [("NAN", 0)]
+
+
+"""
+Currently, these classes are only modified the abstract classes
+"""
 class RiskAvers(Trader):
-    def __init__(self, money=10_100) -> None:
-        super().__init__(money=money, stopLost=0.5)
+    def __init__(self, allTheStocks) -> None:
+        super().__init__(allTheStocks, stopLost=0.05, name="riskAvers   ")
 
 
 class RiskNeutral(Trader):
-    def __init__(self, money=10_100) -> None:
-        super().__init__(money=money, stopLost=0.25)
+    def __init__(self, allTheStocks) -> None:
+        super().__init__(allTheStocks, stopLost=0.02, name="riskNeutral ")
 
 
 class RiskTolerant(Trader):
-    def __init__(self, money=10_100) -> None:
-        super().__init__(money=money, stopLost=0.1)
+    def __init__(self, allTheStocks) -> None:
+        super().__init__(allTheStocks, stopLost=0.01, name="riskTolerant")
 
+
+# TODO, actually write these.
 
 class WildCard(Trader):
-    def __init__(self, money=10_100) -> None:
-        super().__init__(money=money, stopLost=0.25)
+    def __init__(self, allTheStocks) -> None:
+        super().__init__(allTheStocks, stopLost=0.25)
 
     def act(self, listings) -> list:
         return [("NAN",0)]
 
 
 class Oscillator(Trader):
-    def __init__(self, money=10_100) -> None:
-        super().__init__(money=money, stopLost=0.1)
+    def __init__(self, allTheStocks) -> None:
+        super().__init__(allTheStocks, stopLost=0.1)
 
     def act(self, listings) -> list:
         return [("NAN",0)]
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from env import *
 from agent import *
 
-TRADING_DAYS = 1
+TRADING_DAYS = 100
 
 def initialize():
     # set up
@@ -9,19 +9,25 @@ def initialize():
 
 def reportStats(agents):
     print("///////// STATS REPORT /////////")
+    # TODO: for each agent, IDK, whatever stats you want. Crunch the logs I guess?
+    for agent in agents:
+        agent.print(showPortfolio=True)
+
 
 def gameLoop():
-    stockMarket = StockMarket()
-    agents = []
-    agents.extend([RiskNeutral() for _ in range(1)])  # makes x of this type of agent
-    print("stockMarket")
-    for _ in range(TRADING_DAYS+1):
-        todaysListing = stockMarket.getStockListings()
-        for agent in agents:
-            for stock in todaysListing:
-                
-                agent.adjustBalance(stockMarket.doTrade(agent.act(todaysListing)))
-        stockMarket.updateMarket()
+    stockMarket = StockMarket()  # initialize the stock market
+    agents = []  # make a abstract container of agents
+    agents.extend([RiskNeutral(stockMarket.getStockListings()) for _ in range(1)])  # makes x of neutral type of agent
+    agents.extend([RiskTolerant(stockMarket.getStockListings()) for _ in range(1)])  # makes x of tolerant type of agent
+    agents.extend([RiskAvers(stockMarket.getStockListings()) for _ in range(1)])  # makes x of avers type of agent
+    for _ in range(TRADING_DAYS+1): # Trade for x days
+        todaysListing = stockMarket.getStockListings()  # get todays prices
+
+        for agent in agents:  # for each agent
+            for stock in todaysListing:  # for each stock
+                action = agent.act(stock)  # NOTE: Actor's decisions are pass by reference to market
+                # print(action)
+        stockMarket.updateMarket()  # update stock prices for tomorrow
     
     reportStats(agents)
 
